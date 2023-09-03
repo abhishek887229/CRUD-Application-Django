dc0f2b51cbf170d2a367f9b0dd7a5cf0686f182a
abhishek <125186953+abhishek887229@users.noreply.github.com>
2023-09-03 17:25:42 +0530

    make new chnages
    added new application, created forms, login and signup form adding new item webpage.

---
+++ Django/puddle/item/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+
+from .models import Item
+INPUT_CLASS='w-full py-4 px-6 rounded-xl border border-double bg-gray-200'
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model=Item
+        fields=('Category','name','description','price','image')
+
+        widgets={
+            'Category':forms.Select(attrs={
+                'class':INPUT_CLASS
+            }),
+            'name':forms.TextInput(attrs={
+                'class':INPUT_CLASS
+            }),
+            'description':forms.Textarea(attrs={
+                'class':INPUT_CLASS
+            }),
+            'price':forms.TextInput(attrs={
+                'class':INPUT_CLASS
+            }),
+            'image':forms.FileInput(attrs={
+                'class':INPUT_CLASS
+            })
+
+        }        
